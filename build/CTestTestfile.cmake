a73a3d9b90792faca1ed0b75db04c5ac6863bbc8
Grobel Coralie Marie Nicole <grobel@INTRANET.EPFL.CH@SV-UniUbuntu10.intranet.epfl.ch>
2018-11-07 16:42:08 +0100

    changement

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/grobel/Desktop/myfiles/Programmation/Network
+# Build directory: /home/grobel/Desktop/myfiles/Programmation/Network/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Network_simulation "testNetSim")
